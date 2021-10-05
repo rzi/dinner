b501f1ebd3fba727bb5974b12073b02507f2ff24
Rafal Zietak <rzi@vp.pl>
2021-10-05 23:19:25 +0200

    order collection

--- routes/order.js
+++ routes/order.js
@@ -4,17 +4,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  console.log("index get cookie: ", req.signedCookies);
+  console.log("order get cookie: ", req.signedCookies);
   var name = "";
   req.signedCookies ? (name = req.signedCookies.name) : (name = "");
   console.log(`name ${name}`);
 
-  res.render("order", { title: "Express", name: name });
+  res.render("order", { title: "Express", name: name , title1: "2"});
 });
 
 /* Post home page. */
 router.post("/", function (req, res, next) {
-  console.log("Post : index");
+  console.log("Post : order");
   var name = "";
   req.signedCookies ? (name = req.signedCookies.name) : (name = "");
 
@@ -25,8 +25,17 @@ router.post("/", function (req, res, next) {
   console.log("manager " + manager);
   console.log("password " + password);
   console.log(`req.body ${JSON.stringify(req.body)}`); 
-
-  res.render("order", { title: "Express", name: name });
+  var menu = JSON.stringify(req.body);
+  var menu2 = req.body;
+  
+  console.log(`req.body lenght ${menu2.length}`); 
+  // console.log(`req.body parse ${menu2[0]}`); 
+  // console.log(`req.body parse ${JSON.stringify( menu2[1])}`);
+  // console.log(`req.body parse ${JSON.stringify(menu2[1][0][0])}`); 
+for (i=0; i< menu2.length; i++)
+{
+ console.log(`req.body ${JSON.stringify(menu2[i].price)}`); 
+}
+  res.render("order", { title: "Express", name: name , title1: req.body});
 });
-
 module.exports = router;
