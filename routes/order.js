a5bc4128ccf6411fd2f3ce00c69566c4f5fe9219
Rafal Zietak <rzi@vp.pl>
2021-10-06 21:23:19 +0200

    still problem with temp order list

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-// var cookieParser = require("cookie-parser");
+var cookieParser = require("cookie-parser");
+bodyParser = require("body-parser");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,8 +9,9 @@ router.get("/", function (req, res, next) {
   var name = "";
   req.signedCookies ? (name = req.signedCookies.name) : (name = "");
   console.log(`name ${name}`);
-
-  res.render("order", { title: "Express", name: name , title1: req.body});
+  console.log(`req.body ${JSON.stringify(req.body)}`);
+  console.log("cookie: ", req.cookies);
+  res.render("order", { title: "Express", name: name, title1: req.body });
 });
 
 /* Post home page. */
@@ -24,18 +26,25 @@ router.post("/", function (req, res, next) {
   console.log("email " + email);
   console.log("manager " + manager);
   console.log("password " + password);
-  console.log(`req.body ${JSON.stringify(req.body)}`); 
+  console.log(`req.body ${JSON.stringify(req.body)}`);
   var menu = JSON.stringify(req.body);
   var menu2 = req.body;
-  
-  console.log(`req.body lenght ${menu2.length}`); 
-  // console.log(`req.body parse ${menu2[0]}`); 
+
+  console.log(`req.body lenght ${menu2.length}`);
+  // console.log(`req.body parse ${menu2[0]}`);
   // console.log(`req.body parse ${JSON.stringify( menu2[1])}`);
-  // console.log(`req.body parse ${JSON.stringify(menu2[1][0][0])}`); 
-for (i=0; i< menu2.length; i++)
-{
- console.log(`req.body ${JSON.stringify(menu2[i].price)}`); 
-}
-  res.render("order", { title: "Express", name: name , title1: req.body});
+  // console.log(`req.body parse ${JSON.stringify(menu2[1][0][0])}`);
+
+  let options = {
+    maxAge: 600000, // would expire after 1 minutes
+    httpOnly: true,
+    signed: true,
+    secret: "secret",
+  };
+  res.cookie("preOrder", res.body, options);
+  for (i = 0; i < menu2.length; i++) {
+    console.log(`req.body ${JSON.stringify(menu2[i].price)}`);
+  }
+  res.send(req.body);
 });
 module.exports = router;
